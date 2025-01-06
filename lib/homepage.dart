6692597b7edffa5b016bfba927fb33ab62a3d7d9
hridoykhan51 <hridoykhan6786@gmail.com>
2025-01-06 20:36:09 +0600

    complete

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,19 +1,67 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_x/home_page-controllar.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({super.key});
-
+  Homepage({super.key});
+  final HomePagecontrollar _homePagecontrollar = Get.put(HomePagecontrollar());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Count (Getx)"),
+        backgroundColor: Colors.black,
+        title: Text(
+          "Count (Getx)",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-      body: GetBuilder(builder: (_) {
-        return Container();
-        
+      body: GetBuilder<HomePagecontrollar>(builder: (_) {
+        return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Center(
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                  color: Colors.black, borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                child: AnimatedTextKit(
+                    pause: Duration(milliseconds: 1),
+                    repeatForever: true,
+                    animatedTexts: [
+                      FadeAnimatedText(_homePagecontrollar.number.toString(),
+                          textStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                          )),
+                    ]),
+              ),
+            ),
+          )
+        ]);
       }),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: FloatingActionButton(
+                  backgroundColor: Colors.red,
+                  child: Text('-'),
+                  onPressed: _homePagecontrollar.decrement),
+            ),
+            FloatingActionButton(
+                backgroundColor: Colors.black,
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+                onPressed: _homePagecontrollar.increment),
+          ],
+        ),
+      ),
     );
   }
 }
